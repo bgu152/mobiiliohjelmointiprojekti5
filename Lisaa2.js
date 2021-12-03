a6a6900fe2123fe831b3989ec9407bd5d08776b8
bgu152 <erikrammschmidt@gmail.com>
2021-12-03 12:45:38 +0200

    dropdown funkar

--- Lisaa2.js
+++ Lisaa2.js
@@ -1,68 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useFormik } from 'formik';
+import { Picker } from '@react-native-community/picker'
 import { initializeFirestore } from '@firebase/firestore';
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { Image, StyleSheet, Text, View, Alert, FlatList, SafeAreaView, TouchableOpacity, TextInput } from 'react-native';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, setDoc, doc, collection, getDocs, onSnapshot, itemsSnapshot, itemsCol, addDoc, deleteDoc } from 'firebase/firestore';
 import 'firebase/firestore';
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, View, Alert, FlatList, SafeAreaView, TouchableOpacity, TextInput } from 'react-native';
 import { Input, Button, ListItem, Header, Avatar } from 'react-native-elements';
 import { ButtonGroup } from 'react-native-elements/dist/buttons/ButtonGroup';
-import { Formik } from 'formik';
 
 
 
+const App = () => {
 
+    const kategoriat = [
+        { kategoria: 'housut'},
+        { kategoria: 'takki'},
+        { kategoria: 'housut'},
+        { kategoria: 'kengat'},
+        { kategoria: 'pusero'},
+        { kategoria: 'hame'},
+        { kategoria: 'haalari'},
+        { kategoria: 'mekko'},
+        { kategoria: 'paita'},
+        { kategoria: 'mekko'},
+    ];
 
- export default function Lisaa2(){
-  
-    return (
-        <Formik
-        initialValues={{
+    const formik = useFormik({
+        initialValues: {
             kategoria: '',
             lapsi: '',
-            pituudelle:'',
-            kuvaus:'',
-            lisattypvm:''
-        }}
-        onSubmit={values => console.log(values)}
-      >
-        {({ handleChange, handleBlur, handleSubmit, values }) => (
-          <View>
-            <Input
-              onChangeText={handleChange('kategoria')}
-              placeholder='kategoria'
-              onBlur={handleBlur('kategoria')}
-              value={values.kategoria}
-            />
-             <Input
-              onChangeText={handleChange('lapsi')}
-              placeholder='kuka käyttää?'
-              onBlur={handleBlur('kategoria')}
-              value={values.lapsi}
-            />
-            <Input
-              onChangeText={handleChange('pituudelle')}
-              placeholder='pituudelle?'
-              onBlur={handleBlur('kategoria')}
-              value={values.pituudelle}
-            />
-            <Input
-              onChangeText={handleChange('kuvaus')}
-              placeholder='kuvaus?'
-              onBlur={handleBlur('kuvaus')}
-              value={values.kuvaus}
-            />
-            <Input
-              onChangeText={handleChange('lisattypvm')}
-              placeholder='lisattypvm?'
-              onBlur={handleBlur('lisattypvm')}
-              value={values.lisattypvm}
-            />
-            <Button onPress={handleSubmit} title="Submit" />
+            pituudelle: '',
+            kuvaus: '',
+            lisattypvm: '',
+            vuodenajalle: ''
+        },
+        onSubmit: values => { console.log(values) }
+    });
+    return (
+        <View>
+            <Text>Kategoria</Text>
+            <Picker
+                enabled={true}
+                mode="dropdown"
+                onValueChange={formik.handleChange('kategoria')}
+                selectedValue={formik.values.kategoria}
+            >
+                {kategoriat.map((item) => <Picker.Item
+                        label={item.kategoria}
+                        value={item.kategoria}/>
+                )}
+            </Picker>
 
-          </View>
-        )}
-      </Formik>
-    );
+            <Picker
+                enabled={true}
+                mode="dropdown"
+                onValueChange={formik.handleChange('vuodenajalle')}
+                selectedValue={formik.values.city_name}
+            >
+                <Picker.Item label="Kaikki" value="kaikki" />
+                <Picker.Item label="Talvi" value="talvi" />
+                <Picker.Item label="Kesä" value="kesa" />
+                <Picker.Item label="Syksy ja kevät" value="syksy_kevat" />
+            </Picker>
+
+            <Input
+                placeholder='Lapsen nimi'
+                onChangeText={formik.handleChange('lapsi')}
+            />
+            <Input
+                placeholder='Maksimipituus'
+                onChangeText={formik.handleChange('pituudelle')}
+            />
+            <Input
+                placeholder='kuvaus'
+                onChangeText={formik.handleChange('kuvaus')}
+            />
+
+            
+            <Button
+                mode="contained"
+                title='submit'
+                onPress={formik.handleSubmit}
+            >
+                Enter
+            </Button>
+        </View>
+    )
 }
-  
+export default App;
